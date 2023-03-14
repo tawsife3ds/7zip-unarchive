5a1bc5745d66cd0f24804410c10a25345cc2c0f5
Tawsif F. Rahman <tawsif@eagle3dstreaming.com>
2023-03-14 15:06:33 +0600

    feat: extraction - Add extraction using .exe - Add extraction using bin

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const sevenBin = require('7zip-bin');
 const { extractFull } = require('node-7z')
 
-const pathTo7zip = sevenBin.path7za
-const path = '/usr/share/man/man1/7za.1.gz' // will not work
-const seven = extractFull('./archive.7z', './output/dir/', {
-  $bin: pathTo7zip
+
+const path = 'c:/Program Files/7-Zip/7zG.exe' // will not work
+const seven = extractFull('c:/Program Files/7-Zip/archive.7z', './output/dir/', {
+    $bin: path
 })
+
+
+async function extractWith7zBin() {
+    const pathTo7zip = sevenBin.path7za;
+    const seven = await extractFull('./archive.7z', './output/bin/', {
+        $bin: pathTo7zip
+    });
+    console.log("Extraction Done using 7z-bin");
+
+}
+
+async function extractWith7zExe() {
+    const pathTo7zExe = 'c:/Program Files/7-Zip/7zG.exe' // will only work on windows
+    const seven = await extractFull('./archive.7z', './output/exe/', {
+        $bin: pathTo7zExe
+    })
+    console.log("Extraction Done Using 7z .exe");
+}
